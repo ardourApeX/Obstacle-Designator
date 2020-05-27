f3c2b52a6b5395dd894f3f8b6a43e06a8f9ce5f0
ApeX <joygupta501@gmail.com>
2020-05-27 14:11:30 +0530

    Added PrintBoard Function and Converted data from 1.board to Vector

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -16,24 +16,35 @@ vector<int> ParseLine(const string &S)  //Converting a string into vector
 	}
 	return Vector;
 }
-void ReadBoardFile(string Path) //Taking path of file
+vector<vector<int>> ReadBoardFile(string Path) //Taking path of file
 {
 	ifstream fin; 
 	fin.open(Path);
+	vector<vector<int>> Board;  //Vector comprise of binary input from 1.board
 	if(fin) //If such file exists then...
 	{
 		string Line;
 		while(getline(Fin, Line)){ //Copy the data line by line from object to Line 
-			cout<<Line<<"\n";
+			Board.push_back(ParseLine(Line));
+
 		}
+
 	}
-	else
-		cout<<"No such file found!"<<"\n"; //If such file doesn't exists...
+	return Board;
 	fin.close();
+}
+void PrintBoard(const vector<vector<int>> Board)
+{
+	for(auto I : Board){
+		for(int J : I)
+			cout<<J;
+		cout<<"\n";
+	}
 }
 #include "test.cpp" //for testing
 int main()
 {
-	ReadBoardFile("1.board");
-	TestParseLine();
+	auto Board = ReadBoardFile("1.board");
+	PrintBoard(Board);
+	
 }
